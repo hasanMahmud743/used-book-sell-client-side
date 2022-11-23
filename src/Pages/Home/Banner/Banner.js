9004a9397a332286f4a4fe6ed1c17692a8cd8f53
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-11-24 04:40:07 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <p>This is banner</p>
+        <div className='banner flex  '>
+            <div className='width'>
+                <p className='text-6xl py-10 font-bold'>Getting out the best level</p>
+                <p className='text-2xl  pb-10'>Hey, i am the Mahmudul Hasan profession photographer here to give you a proffesional sports photographic service</p>
+                <button className="btn  text-light  btn-info m-3">Request a Shoot</button>
+                <button className="btn  btn-success m-3">Hire Me</button>
+
+
+                
+            </div>
+
             
         </div>
     );
