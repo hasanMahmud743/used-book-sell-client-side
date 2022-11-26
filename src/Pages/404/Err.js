63bd838bac538a08f8e7d16233bef9bbdfd28d63
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-11-26 18:50:08 +0600

    dashboard updated as per user

--- src/Pages/404/Err.js
+++ src/Pages/404/Err.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import useTitle from '../../Hooks/UseTitle';
 
 const Err = () => {
+    useTitle('Error page')
     return (
         <div className='border text-center'>
             <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg" alt="404" />
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { authContext } from "../../Context/Contexts";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const Dashboard = () => {
+  const [users, setUsers] = useState("");
+  const { user } = useContext(authContext);
+  console.log(user?.email);
+
+  useEffect(() => {
+    fetch(`http://localhost:6500/allUser?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.accountType);
+        setUsers(data);
+      });
+  }, [user?.email]);
+
   return (
     <div>
       <Navbar></Navbar>
@@ -15,29 +29,45 @@ const Dashboard = () => {
           >
             Open drawer
           </label>
-         <div className="p-5"> <Outlet></Outlet></div>
+          <div className="p-5">
+            {" "}
+            <Outlet></Outlet>
+          </div>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-            <li>
-              <Link to='/dashboard/allusers'>All Orders</Link>
-            </li>
-            <li>
-              <Link to='/dashboard/addproducts'> Add a Product</Link>
-            </li>
-            <li>
-            <Link to='/dashboard/myproduct'> My Product</Link>
-            </li>
-            <li>
-            <Link to='/dashboard/allseller'> All Seller</Link>
-            </li>
-            <li>
-            <Link to='/dashboard/allbuyer'> All Buyer</Link>
-            </li>
-            <li>
-              <a>Reported Item</a>
-            </li>
+            
+            {users?.accountType === "normal" && (
+              <li>
+                <Link to="/dashboard/allusers">All Orders</Link>
+              </li>
+            )}
+
+            {users?.accountType === "seller" && (
+              <>
+                <li>
+                  <Link to="/dashboard/addproducts"> Add a Product</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard/myproduct"> My Product</Link>
+                </li>
+              </>
+            )}
+
+            {users?.accountType === "admin" && (
+              <>
+                <li>
+                  <Link to="/dashboard/allseller"> All Seller</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard/allbuyer"> All Buyer</Link>
+                </li>
+                <li>
+                  <a>Reported Item</a>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
